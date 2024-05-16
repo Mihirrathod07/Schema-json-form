9ffa65ecccb24e24d0fb30d416444c835357c84d
Mihir Rathod <mihir8716@gmail.com>
2024-05-16 15:45:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from '@rjsf/core';
+import schema from './component/Schema.json';
+import validator from '@rjsf/validator-ajv6';
 
 function App() {
+  const onSubmit = ({ formData }) => {
+    console.log("Form data submitted: ", formData);
+  };
+
+  // Custom validation function
+  const validate = (formData, errors) => {
+    if (formData.age && (formData.age < 0 || formData.age > 120)) {
+      errors.age.addError("Age must be between 0 and 120.");
+    }
+    if (formData.password && formData.password.length < 3) {
+      errors.password.addError("Password must be at least 3 characters long.");
+    }
+    if (formData.telephone && formData.telephone.length < 10) {
+      errors.telephone.addError("Telephone must be at least 10 digits long.");
+    }
+    return errors;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Registration Form</h1>
+      <Form 
+        schema={schema} 
+        validator={validator}
+        validate={validate}
+        onSubmit={onSubmit} 
+      />
     </div>
   );
 }
